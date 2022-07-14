4fda46bb43ed5fe278152f2cb876e588f8db42fe
Boris Shapovalov <bashap@bk.ru>
2022-07-14 18:12:26 +0300

    add logic predicat

--- 002.py
+++ 002.py
@@ -5,5 +5,4 @@ result = True
 for x in 1, 0:
     for y in 1, 0:
         for z in 1, 0:
-            print(
-                f"{x = } {y = } {z = }  {not(x or y or z) == (not x and not y and not z)}")
+            print(f"{x = } {y = } {z = }  {not(x or y or z) == (not x and not y and not z)}")
