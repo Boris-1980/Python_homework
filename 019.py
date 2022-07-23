e3697ad39a065c9a894adc39bd5d85939b15c07e
Boris Shapovalov <bashap@bk.ru>
2022-07-23 12:48:51 +0300

    polynomial

---
+++ 019.py
@@ -0,0 +1,30 @@
+# Задана натуральная степень k. 
+# Сформировать случайным образом список коэффициентов (значения от 0 до 100) 
+# многочлена и записать в файл многочлен степени k.
+# Пример:
+# - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+import random
+import pathlib
+
+k = int(input('Введите коэффициент: '))
+a = int(random.randint(0,100))
+b = int(random.randint(0,100))
+c = int(random.randint(0,100))
+
+if a != 0:
+    first = (str(a) + "x^" + str(k) + " + ")
+else:
+    first = (str())
+
+if b != 0:
+    second = (str(b) + "x" + " + ")
+else:
+    second = (str())
+
+if c != 0:
+    third = (str(c) + " = 0")
+else:
+    third = (str())
+
+print(f'{first}{second}{third}')
